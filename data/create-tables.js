6481b75205c7a553fb7e2beff7f0805a6185e034
BenFrancka <benfrancka@gmail.com>
2021-06-16 12:38:25 -0700

    updates create-tables.js to reflect seed data

--- data/create-tables.js
+++ data/create-tables.js
@@ -17,10 +17,14 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE meals (
                     id SERIAL PRIMARY KEY NOT NULL,
                     name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
+                    in_stock BOOLEAN NOT NULL,
+                    description VARCHAR(512) NOT NULL,
+                    category VARCHAR(512) NOT NULL,
+                    difficulty VARCHAR(512) NOT NULL,
+                    price INTEGER NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
